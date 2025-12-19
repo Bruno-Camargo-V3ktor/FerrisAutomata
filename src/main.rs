0ab6447d644714a9697a83a7df8c052af3f02200
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2025-12-19 14:04:38 -0300

    refactor: move test for separator mod

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,39 @@
-use crate::{ machine::Machine, state::State, symbol::Symbol };
-
 mod machine;
 mod state;
 mod symbol;
 mod process;
 
-fn main() {
-    let q0 = State::new("q0", false);
-    let q1 = State::new("q1", false);
-    let q2 = State::new("q2", false);
-    let q3 = State::new("q3", true);
+fn main() {}
 
-    q0.add_transaction(Symbol::Letter('a'), &q1);
-    q0.add_transaction(Symbol::Letter('b'), &q2);
+#[cfg(test)]
+mod tests {
+    use crate::{ machine::Machine, state::State, symbol::Symbol };
 
-    q1.add_transaction(Symbol::Letter('a'), &q3);
-    q1.add_transaction(Symbol::Letter('b'), &q2);
+    #[test]
+    fn test_afd_01() {
+        let q0 = State::new("q0", false);
+        let q1 = State::new("q1", false);
+        let q2 = State::new("q2", false);
+        let q3 = State::new("q3", true);
 
-    q2.add_transaction(Symbol::Letter('a'), &q1);
-    q2.add_transaction(Symbol::Letter('b'), &q3);
+        q0.add_transaction(Symbol::Letter('a'), &q1);
+        q0.add_transaction(Symbol::Letter('b'), &q2);
 
-    q3.add_transaction(Symbol::Letter('a'), &q3);
-    q3.add_transaction(Symbol::Letter('b'), &q3);
+        q1.add_transaction(Symbol::Letter('a'), &q3);
+        q1.add_transaction(Symbol::Letter('b'), &q2);
 
-    let machine = Machine::new(&q0);
+        q2.add_transaction(Symbol::Letter('a'), &q1);
+        q2.add_transaction(Symbol::Letter('b'), &q3);
 
-    let res = machine.analyze("aaba");
-    println!("{res}");
+        q3.add_transaction(Symbol::Letter('a'), &q3);
+        q3.add_transaction(Symbol::Letter('b'), &q3);
 
-    let res = machine.analyze("abab");
-    println!("{res}");
+        let machine = Machine::new(&q0);
+
+        let res = machine.analyze("aaba");
+        assert_eq!(res, true);
+
+        let res = machine.analyze("abab");
+        assert_eq!(res, false);
+    }
 }
