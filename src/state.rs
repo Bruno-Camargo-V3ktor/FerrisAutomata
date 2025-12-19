13438bd8f6c48066a877a5dcc9b0955a306b218c
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2025-12-19 13:12:56 -0300

    refactor: convert field transaction in a RwLock and create function get_transaction

--- src/state.rs
+++ src/state.rs
@@ -1,13 +1,11 @@
 use crate::symbol::Symbol;
-use std::{
-    collections::HashMap,
-    sync::{Arc, Weak},
-};
+use std::{ collections::HashMap, sync::{ Arc, RwLock, RwLockReadGuard, Weak } };
 
+#[allow(dead_code)]
 #[derive(Debug)]
 pub struct State {
     pub name: String,
-    pub transactions: HashMap<Symbol, Vec<Weak<State>>>,
+    pub transactions: RwLock<HashMap<Symbol, Vec<Weak<State>>>>,
     pub finishing: bool,
 }
 
@@ -15,15 +13,29 @@ impl State {
     pub fn new(name: impl Into<String>, finishing: bool) -> Arc<Self> {
         let state = Self {
             name: name.into(),
-            transactions: HashMap::new(),
+            transactions: RwLock::new(HashMap::new()),
             finishing,
         };
 
         Arc::new(state)
     }
 
-    pub fn add_transaction(&mut self, symbol: Symbol, state: Weak<State>) {
-        let value = self.transactions.entry(symbol).or_default();
-        value.push(state);
+    pub fn add_transaction(&self, symbol: Symbol, state: &Arc<State>) {
+        match self.transactions.write() {
+            Ok(mut table) => {
+                let list = table.entry(symbol).or_default();
+                list.push(Arc::downgrade(state));
+            }
+            Err(err) => panic!("{err}"),
+        };
+    }
+
+    pub fn get_transaction(&self) -> RwLockReadGuard<'_, HashMap<Symbol, Vec<Weak<State>>>> {
+        match self.transactions.read() {
+            Ok(table) => {
+                return table;
+            }
+            Err(err) => panic!("{err}"),
+        };
     }
 }
