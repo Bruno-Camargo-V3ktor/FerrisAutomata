0a5b6cee93c4c26aacf00be32e7ab3e34dd544f5
Bruno-Camargo-V3ktor <bruno.ferreira.v3ktor@gmail.com>
2025-12-21 00:53:08 -0300

    feat: create function open file, and return vetor contains lines for file

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,5 +1,6 @@
 use colorz::Colorize;
-use std::{env::args, process::ExitCode};
+use std::io::{Read, Result as IOResult};
+use std::{env::args, fs::File, process::ExitCode};
 
 fn main() -> ExitCode {
     let args: Vec<String> = args().skip(1).collect();
@@ -10,5 +11,23 @@ fn main() -> ExitCode {
         return ExitCode::FAILURE;
     }
 
+    let lines = match open_file(&args[0]) {
+        Ok(lines) => lines,
+        Err(err) => {
+            eprintln!("{} {:?}", "File Invalid...\n Error:".red(), err);
+            return ExitCode::FAILURE;
+        }
+    };
+
     ExitCode::SUCCESS
+}
+
+pub fn open_file(path: impl Into<String>) -> IOResult<Vec<String>> {
+    let mut file = File::open(path.into())?;
+    let mut buffer = String::new();
+
+    file.read_to_string(&mut buffer)?;
+    let lines = buffer.lines();
+
+    Ok(lines.into_iter().map(|str| str.to_string()).collect())
 }
